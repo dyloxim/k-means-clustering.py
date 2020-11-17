be2d7eb566af9a01d93db832149d0a06985008fc
Joel Strouts <joelstrouts@gmail.com>
2020-11-17 19:02:13 +0000

    Organise code into blocks of related functions

--- k-means-clustering.py
+++ k-means-clustering.py
@@ -1,7 +1,12 @@
 # main.py
+
+# BEGIN: Dependencies
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+# END: Dependencies
+
+# BEGIN: Utility Functions
 
 
 def squared_dist(u, v):
@@ -11,21 +16,52 @@ def squared_dist(u, v):
     return running_total
 
 
+def centroid_of(coords):
+    N = len(coords[0])
+    centroid = np.empty(N)
+    for i, _ in enumerate(coords[0]):
+        centroid[i] = np.average([x[i] for x in coords])
+    return centroid
+
+
 def by_components(vecs):
     indicies = range(0, len(vecs[0]))
     components = [np.array([]) for i in indicies]
     for i in indicies:
         components[i] = [vec[i] for vec in vecs]
     return components
+# END: Utility Functions
+
+
+# BEGIN: State Functions
+def init_state():
+    global state
+    state = {
+        'coords': np.array(np.random.rand(1500, 2), dtype=np.float64),
+        'centroids': np.array(np.random.rand(15, 2), dtype=np.float64),
+        'clusterings': None
+    }
+    state['clusterings'] = [np.array([], dtype=int)
+                            for i in range(state_meta('k'))]
+
+
+def state_meta(key):
+    global state
+    return {
+        'k': len(state['centroids']),
+        'clusters': [cluster(i) for i in range(len(state['centroids']))]
+    }[key]
 
 
 def cluster(i):
     global state
     return [] if state['clusterings'] is None else [state['coords'][coord_idx]
-          for coord_idx, cluster_idx in enumerate(state['clusterings'])
-          if cluster_idx == i]
+                                                    for coord_idx, cluster_idx in enumerate(state['clusterings'])
+                                                    if cluster_idx == i]
+# END: State Functions
 
 
+# BEGIN: Algorithm
 def update_clusterings():
     global state
     state['clusterings'] = np.empty(len(state['coords']), dtype=int)
@@ -44,52 +80,14 @@ def update_centroids():
                           for i, _ in enumerate(state['centroids'])]
 
 
-def centroid_of(coords):
-    N = len(coords[0])
-    centroid = np.empty(N)
-    for i, _ in enumerate(coords[0]):
-        centroid[i] = np.average([x[i] for x in coords])
-    return centroid
-
-
-def init_state():
-    global state 
-    state = {
-        'coords': np.array(np.random.rand(1500,2), dtype=np.float64),
-        'centroids': np.array(np.random.rand(15,2), dtype=np.float64),
-        'clusterings': None
-    }
-    state['clusterings'] = [np.array([], dtype=int) for i in range(state_meta('k'))]
-
-
-def state_meta(key):
+def next_state():
     global state
-    return {
-        'k': len(state['centroids']),
-        'clusters': [cluster(i) for i in range(len(state['centroids']))]
-    }[key]
+    update_clusterings()
+    update_centroids()
+# END: Algorithm
 
 
-def animate(i):
-    global state, ax, subplots
-    ax.set_xlabel("Frame " + str(i))
-    if i == 0:
-        init_state()
-        init_fig()
-    else:
-        next_state()
-        update_subplots()
-
-
-def update_subplots():
-    global state, subplots
-    subplots['coords'].set_offsets(state['coords'])
-    for i, cluster_plot in enumerate(subplots['clusters']):
-        if state_meta('clusters')[i] != []:
-            cluster_plot.set_offsets(state_meta('clusters')[i])
-    subplots['centroids'].set_offsets(state['centroids'])
-
-
+# BEGIN: Plotting
 def init_fig():
     global ax, subplots
     ax.clear()
@@ -103,21 +101,36 @@ def init_fig():
     ax.set_xlabel("Frame 0")
     update_subplots()
 
-state = None
+
+def update_subplots():
+    global state, subplots
+    subplots['coords'].set_offsets(state['coords'])
+    for i, cluster_plot in enumerate(subplots['clusters']):
+        if state_meta('clusters')[i] != []:
+            cluster_plot.set_offsets(state_meta('clusters')[i])
+    subplots['centroids'].set_offsets(state['centroids'])
+
+
+def animate(i):
+    global state, ax, subplots
+    ax.set_xlabel("Frame " + str(i))
+    if i == 0:
+        init_state()
+        init_fig()
+    else:
+        next_state()
+        update_subplots()
+
 
 def run():
     global state, ax, subplots
     init_state()
     fig = plt.figure()
     ax = fig.add_subplot(xlim=(-1, 4), ylim=(-1, 4))
-    ani = FuncAnimation(fig, animate, frames=range(6), repeat=True, interval=800)
+    ani = FuncAnimation(fig, animate, frames=range(15),
+                        repeat=True, interval=800)
     plt.show()
-
-
-def next_state():
-    global state
-    update_clusterings()
-    update_centroids()
+# END: Plotting
 
 
 run()
